b5489eec2d162c25b9a7df580e12862d2cc61b1c
Aleksandar <jasarevic977@gmail.com>
2020-08-10 00:57:42 +0200

    Install required libraries

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import React from 'react';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 function App() {
   return (
-    <div className="App">
-      App komponenta
-    </div>
+    <Container>
+      <FontAwesomeIcon icon={ faHome } /> Home
+    </Container>
   );
 }
 
