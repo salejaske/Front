efa92a77e61e21fd8a3e26d46bc2114713150d43
Aleksandar <jasarevic977@gmail.com>
2020-08-12 23:51:48 +0200

    Implement a basic category page component with data acquisition simulation

--- src/index.tsx
+++ src/index.tsx
@@ -12,11 +12,18 @@ import { MainMenu, MainMenuItem } from './components/MainMenu/MainMenu';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import ContactPage from './components/ContactPage/ContactPage';
 import UserLoginPage from './components/UserLoginPage/UserLoginPage';
+import CategoryPage from './components/CategoryPage/CategoryPage';
 
 const menuItems = [
   new MainMenuItem("Home", "/"),
   new MainMenuItem("Contact", "/contact/"),
   new MainMenuItem("Log in", "/user/login/"),
+
+  
+  new MainMenuItem("Cat 1", "/category/1/"),
+  new MainMenuItem("Cat 7", "/category/7/"),
+  new MainMenuItem("Cat 21", "/category/21/"),
+  
 ];
  
 ReactDOM.render(
@@ -27,6 +34,7 @@ ReactDOM.render(
         <Route exact path="/" component={ HomePage } />
         <Route path="/contact" component={ ContactPage } />
         <Route path="/user/login" component={ UserLoginPage } />
+        <Route path="/category/:cId" component={ CategoryPage } />
       </Switch>
     </HashRouter>
 
---
+++ src/types/CategoryType.ts
@@ -0,0 +1,8 @@
+import ArticleType from "./ArticleType";
+
+export default class CategoryType{
+    categoryId?: number;
+    name?: string;
+    items?: ArticleType[];
+
+}
