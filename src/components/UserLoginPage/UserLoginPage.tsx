614966969883f7aa765cdb335e2ee04650d5bb25
Aleksandar <jasarevic977@gmail.com>
2020-08-15 00:20:40 +0200

    Zavrseni klipovi od 78-80.

--- src/components/UserLoginPage/UserLoginPage.tsx
+++ src/components/UserLoginPage/UserLoginPage.tsx
@@ -1,23 +1,116 @@
 import React from "react"
-import { Container, Card } from "react-bootstrap"
+import { Container, Card, Form, Button, Col, Alert } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSignInAlt } from "@fortawesome/free-solid-svg-icons"
+import api, { ApiResponse, saveToken, saveRefreshToken } from '../../api/api'
+import { Redirect } from "react-router-dom"
+
+interface UserLoginPageState{
+    email: string;
+    password: string;
+    errorMessage: string;
+    isLoggedIn: boolean;
+}
 
 export default class UserLoginPage extends React.Component{
+    state: UserLoginPageState;
+
+    constructor(props: Readonly<{}>){
+        super(props);
+
+        this.state={
+            email: '',
+            password: '',
+            errorMessage: '',
+            isLoggedIn: false,
+        }
+
+    }
+
+    private formInputChanged(event: React.ChangeEvent<HTMLInputElement>){
+        const newState = Object.assign(this.state, {
+            [ event.target.id ]: event.target.value
+        });
+
+        this.setState(newState);
+    }
+
+    private setErrorMessage(message: string ){
+        const newState = Object.assign(this.state,{
+            errorMessage: message,
+        });
+        this.setState(newState);
+    }  
+    
+    private setLogginState(isLoggedIn: boolean){
+        const newState = Object.assign(this.state,{
+            isLoggedIn: isLoggedIn,
+        });
+        this.setState(newState);
+    }
+
+    private doLogin(){
+        api('auth/user/login', 'post', {
+            email: this.state.email,
+            password: this.state.password,
+        })
+        .then((res: ApiResponse) => {
+            if(res.status === 'error'){
+                this.setErrorMessage('System error... Try again')
+                return;
+            }
+
+            if(res.status === 'ok'){
+                
+                saveToken(res.data.token);
+                saveRefreshToken(res.data.refreshToken);
+
+                this.setLogginState(true);
+            }
+        });
+    }
+
     render(){
-        return(
+        if (this.state.isLoggedIn === true){
+            return (
+                <Redirect to="/" />
+            );
+        }
+        return( 
         <Container>
+            <Col md= { { span: 6, offset: 3 } }>
             <Card>
                 <Card.Body>
                     <Card.Title>
                     <FontAwesomeIcon icon={ faSignInAlt } /> User Login
                     </Card.Title>
-                    <Card.Text>
-                        ... the form will be shown here...
-                    </Card.Text>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label htmlFor="email">E-mail:</Form.Label>
+                                <Form.Control type="email" id="email"
+                                              value={ this.state.email }
+                                              onChange={ event => this.formInputChanged(event as any) } />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="password">Password:</Form.Label>
+                                <Form.Control type="password" id="password"
+                                              value={ this.state.password }
+                                              onChange={ event => this.formInputChanged(event as any) }  />
+                            </Form.Group>
+                            <Form.Group>
+                                <Button variant="primary"
+                                        onClick={ () => this.doLogin() }>
+                                    Log in
+                                </Button>
+                            </Form.Group>
+                        </Form>
+                        <Alert variant="danger"
+                               className={ this.state.errorMessage ? '' : 'd-none'}>
+                            { this.state.errorMessage}
+                        </Alert>
                 </Card.Body>
             </Card>
-            
+            </Col>
         </Container>
         );
     }
